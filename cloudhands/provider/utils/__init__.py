9a8d7570444bb0f8f57876468d4782efb02a1952
pjkersha <philip.kershaw@stfc.ac.uk>
2014-03-24 22:22:38 +0000

    Porting code from cloudhands ops functional test for edge device

---
+++ cloudhands/provider/utils/__init__.py
@@ -0,0 +1,70 @@
+"""JASMIN Cloud
+
+Cloudhands Provider Interface package - utility functions for converting
+variables
+"""
+__author__ = "P J Kershaw"
+__date__ = "24/03/14"
+__copyright__ = "(C) 2014 Science and Technology Facilities Council"
+__license__ = "BSD - see LICENSE file in top-level directory"
+__revision__ = "$Id$"
+
+is_bool = lambda val: val.lower() in ('true', 'false')
+bool2str = lambda val: str(val).lower()
+
+def infer_type_from_str(val):
+    '''Attempt to convert a string to its correct equivalent type by checking
+    its content.
+    
+    :ivar val: string to be converted
+    :type val: basestring
+    :return: infered value
+    :rtype: basestring / long / bool or float  
+    '''
+    if is_bool(val):
+        return bool(val)
+    
+    # Try for an integer
+    try:
+        return long(val)
+ 
+    except ValueError:
+        # Check for floating point number
+        try:
+            return float(val)
+        
+        except ValueError:
+            # Default to string
+            return val
+
+
+def mk_valid_varname(name):
+    '''Make a valid Python variable name from XML element attributes
+    
+    :ivar name: XML variable name to be converted
+    :type name: basestring
+    :return: equivalent name in lower case with underscores
+    :rtype: basestring
+    '''
+    if not isinstance(name, basestring):
+        return None
+    
+    varname = camelcase2underscores(re.sub('[^0-9a-zA-Z_]', '_', name))
+
+    # Avoid reserved names
+    if keyword.iskeyword(varname):
+        varname += '_'
+        
+    return varname
+
+
+def camelcase2underscores(varname):
+    '''Convert camel case variable names to underscore equivalent
+    
+    :ivar varname: camel case variable name to be converted
+    :type varname: basestring
+    :return: equivalent name in lower case with underscores
+    :rtype: basestring
+    '''
+    to_underscores_name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', varname)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', to_underscores_name).lower()
