ac610863f6baf09b18ef33085dd944c21517b29c
pjkersha <philip.kershaw@stfc.ac.uk>
2014-04-01 17:04:21 +0100

    Added namespace declarations for namespace packages and functional tests module

---
+++ cloudhands/provider/vcloud/__init__.py
@@ -0,0 +1,18 @@
+"""JASMIN Cloud
+
+Cloudhands Provider Interface package - vCloud sub-package
+
+This is a setuptools namespace_package.  DO NOT place any other
+code in this file!  There is no guarantee that it will be installed
+with easy_install.  See:
+
+http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
+
+... for details.
+"""
+__author__ = "P J Kershaw"
+__date__ = "01/04/14"
+__copyright__ = "(C) 2014 Science and Technology Facilities Council"
+__license__ = "BSD - see LICENSE file in top-level directory"
+__revision__ = "$Id$"
+__import__('pkg_resources').declare_namespace(__name__)
